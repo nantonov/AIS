43985afac95afdee49463966fdfe382ffaa08f2f
VadimZharikov <vadzhar@mail.ru>
2021-10-14 12:59:37 +0300

    Add di, database config and nuget package

---
+++ AIS/AIS.DAL/DI/DataAccessRegister.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AIS.DAL.DI
+{
+    public static class DataAccessRegister
+    {
+        public static void AddDataAccess(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DatabaseContext>(op =>
+                {
+                    op.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+                }
+            );
+        }
+    }
+}
---
+++ AIS/AIS.DAL/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AIS.DAL
+{
+    class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            if (Database.IsRelational())
+            {
+                Database.Migrate();
+            }
+        }
+
+        public DatabaseContext()
+        {
+            Database.Migrate();
+        }
+
+        //add custom field configuration via IEntityTypeConfiguration
+        /*protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(<mapping_config>).Assembly);
+        }*/
+    }
+}
