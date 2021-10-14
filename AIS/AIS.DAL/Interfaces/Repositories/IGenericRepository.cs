06cab9e9be52c18c2fc0907916b6d47161cfda5d
VadimZharikov <vadzhar@mail.ru>
2021-10-14 13:12:48 +0300

    Add generic repository

---
+++ AIS/AIS.DAL/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AIS.DAL.Interfaces.Repositories
+{
+    interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> Add(TEntity item);
+        Task<IEnumerable<TEntity>> Get();
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
+        Task<TEntity> GetById(int id);
+        Task<TEntity> Update(TEntity item);
+        Task Delete(TEntity item);
+    }
+}
