9e79ea0cce6b6ad2de145d5a38e4dc1c953461fd
VadimZharikov <vadzhar@mail.ru>
2021-10-15 12:27:43 +0300

    migrations

---
+++ AIS/AIS.DAL/Migrations/20211015092557_Interviewee.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AIS.DAL.Migrations
+{
+    public partial class Interviewee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Interviewees",
+                columns: table => new
+                {
+                    IntervieweeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AppliesFor = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interviewees", x => x.IntervieweeId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Interviewees");
+        }
+    }
+}
