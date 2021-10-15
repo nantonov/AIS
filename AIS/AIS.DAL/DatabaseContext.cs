c3b5b93525b0d41c0b41ff4ad6ced5b752fe37a3
VadimZharikov <vadzhar@mail.ru>
2021-10-15 13:16:09 +0300

    add company entity

--- AIS/AIS.DAL/DatabaseContext.cs
+++ AIS/AIS.DAL/DatabaseContext.cs
@@ -20,6 +20,7 @@ namespace AIS.DAL
 
         public DbSet<IntervieweeEntity> Interviewees { get; set; }
         public DbSet<EmployeeEntity> Employees { get; set; }
+        public DbSet<CompanyEntity> Companies { get; set; }
 
         //add custom field configuration via IEntityTypeConfiguration
         protected override void OnModelCreating(ModelBuilder modelBuilder)
---
+++ AIS/AIS.DAL/Entities/CompanyEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AIS.DAL.Entities
+{
+    public class CompanyEntity
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<EmployeeEntity> Employees { get; set; }
+        public IEnumerable<IntervieweeEntity> Interviewees { get; set; }
+    }
+}
