426235d14655406ad9838253916b4ad8b53395d4
pashkadun <shkadunpavel@gmail.com>
2021-10-19 15:19:44 +0300

    Add tests for validators.

---
+++ AIS/AIS.API.Tests/Validators/ChangeEmployeeViewModelValidatorTests.cs
@@ -0,0 +1,99 @@
+﻿using AIS.API.Validators;
+using AIS.API.ViewModels.Employee;
+using Xunit;
+
+namespace AIS.API.Tests.Validators
+{
+    public class ChangeChangeEmployeeViewModelValidatorTests
+    {
+        [Fact]
+        public void Validate_ShouldReturnTrue_WithValidModel()
+        {
+            var validator = new ChangeEmployeeViewModelValidator();
+
+            var model = new ChangeEmployeeViewModel
+            {
+                Name = "asdasd",
+                CompanyId = 1
+            };
+
+            validator.Validate(model);
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+        
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWithoutName()
+        {
+            var validator = new ChangeEmployeeViewModelValidator();
+
+            var model = new ChangeEmployeeViewModel
+            {
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWithoutCompanyId()
+        {
+            var validator = new ChangeEmployeeViewModelValidator();
+
+            var model = new ChangeEmployeeViewModel
+            {
+                Name = "asdasd"
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWhenCompanyIdLessThanOne()
+        {
+            var validator = new ChangeEmployeeViewModelValidator();
+
+            var model = new ChangeEmployeeViewModel
+            {
+                Name = "asdasd",
+                CompanyId = 0
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWhenNameLengthLessThanThree()
+        {
+            var validator = new ChangeEmployeeViewModelValidator();
+
+            var model = new ChangeEmployeeViewModel
+            {
+                Name = "as",
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}
---
+++ AIS/AIS.API.Tests/Validators/EmployeeViewModelValidatorTests.cs
@@ -0,0 +1,139 @@
+﻿using AIS.API.Validators;
+using AIS.API.ViewModels.Employee;
+using Xunit;
+
+namespace AIS.API.Tests.Validators
+{
+    public class EmployeeViewModelValidatorTests
+    {
+        [Fact]
+        public void Validate_ShouldReturnTrue_WithValidModel()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 2,
+                Name = "asdasd",
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWithoutId()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Name = "asdasd",
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWithoutName()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 2,
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWithoutCompanyId()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 2,
+                Name = "asdasd"
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWhenIdLessThanOne()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 0,
+                Name = "asdasd",
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWhenCompanyIdLessThanOne()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 2,
+                Name = "asdasd",
+                CompanyId = 0
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+
+        [Fact]
+        public void Validate_ShouldReturnFalse_WithInvalidModelWhenNameLengthLessThanThree()
+        {
+            var validator = new EmployeeViewModelValidator();
+
+            var model = new EmployeeViewModel
+            {
+                Id = 2,
+                Name = "as",
+                CompanyId = 1
+            };
+
+            // Act
+            var result = validator.Validate(model);
+
+            // Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}
