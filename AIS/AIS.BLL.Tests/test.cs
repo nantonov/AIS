3238f06de08384a5b1a7804128cf30f75f55a784
pashkadun <shkadunpavel@gmail.com>
2021-10-25 13:48:29 +0300

    Add second test test for CompanyRepository.

--- AIS/AIS.BLL.Tests/test.cs
+++ AIS/AIS.BLL.Tests/test.cs
@@ -23,6 +23,39 @@ namespace AIS.BLL.Tests
             {
                 add.Companies.Add(new CompanyEntity
                 {
+                    Id = 13,
+                    Name = "asd"
+                });
+
+                add.Companies.Add(new CompanyEntity
+                {
+                    Id = 16,
+                    Name = "dsa"
+                });
+
+                await add.SaveChangesAsync();
+            }
+
+            var repository = new CompanyRepository(context);
+            var employee = repository.GetById(13, default);
+
+            employee.Result.ShouldNotBeNull();
+            employee.Result.Name.ShouldBeEquivalentTo("asd");
+        }
+
+        [Fact]
+        public async Task GetInterviewee_ReturnNull()
+        {
+            var options = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(databaseName: "EmployeeDataBase")
+                .Options;
+
+            var context = new DatabaseContext(options);
+
+            using (var add = new DatabaseContext(options))
+            {
+                add.Companies.Add(new CompanyEntity
+                {
                     Id = 12,
                     Name = "asd"
                 });
@@ -37,10 +70,9 @@ namespace AIS.BLL.Tests
             }
 
             var repository = new CompanyRepository(context);
-            var employee = repository.GetById(12, default);
+            var employee = repository.GetById(14, default);
 
-            await employee.ShouldNotBeNull();
-            employee.Result.Name.ShouldBeEquivalentTo("asd");
+            employee.Result.ShouldBeNull();
         }
     }
 }
