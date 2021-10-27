d2d575f521c8302eab8ab54fed5dc7de1106bb03
VadimZharikov <vadzhar@mail.ru>
2021-10-27 14:21:42 +0300

    QuestionArea tests

--- AIS/AIS.API/ViewModels/Company/CompanyViewModel.cs
+++ AIS/AIS.API/ViewModels/Company/CompanyViewModel.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections.Generic;
-using AIS.API.ViewModels.Employee;
-using AIS.API.ViewModels.Interviewee;
-
-namespace AIS.API.ViewModels.Company
+﻿namespace AIS.API.ViewModels.Company
 {
     public class CompanyViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public IEnumerable<EmployeeViewModel> Employees { get; set; }
-        public IEnumerable<IntervieweeViewModel> Interviewees { get; set; }
     }
 }
