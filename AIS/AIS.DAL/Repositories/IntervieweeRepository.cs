6adb98d8b9c517eceaf603bdbfd3d0e56356f376
pashkadun <shkadunpavel@gmail.com>
2021-10-27 10:19:00 +0300

    Update

--- AIS/AIS.DAL/Repositories/IntervieweeRepository.cs
+++ AIS/AIS.DAL/Repositories/IntervieweeRepository.cs
@@ -15,17 +15,17 @@ namespace AIS.DAL.Repositories
         {
         }
         
-        public async Task<IEnumerable<IntervieweeEntity>> Get(CancellationToken ct)
+        public override async Task<IEnumerable<IntervieweeEntity>> Get(CancellationToken ct)
         {
             return await _context.Interviewees.Include(x => x.Company).ToListAsync(ct);
         }
 
-        public IEnumerable<IntervieweeEntity> Get(Func<IntervieweeEntity, bool> predicate, CancellationToken ct)
+        public override IEnumerable<IntervieweeEntity> Get(Func<IntervieweeEntity, bool> predicate, CancellationToken ct)
         {
             return _context.Interviewees.Include(x => x.Company).Where(predicate).ToList();
         }
 
-        public async Task<IntervieweeEntity> GetById(int id, CancellationToken ct)
+        public override async Task<IntervieweeEntity> GetById(int id, CancellationToken ct)
         {
             var entity = await _context.Interviewees.Include(x => x.Company).FirstOrDefaultAsync(x => x.Id == id, ct);
 
