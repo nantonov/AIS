806c12b80b35d9041e610d4fd2566f73f3229236
Blaackolf931 <denis.kagukin@gmail.com>
2021-10-29 11:25:18 +0300

    update startup

--- AIS/AIS.API/Startup.cs
+++ AIS/AIS.API/Startup.cs
@@ -23,10 +23,12 @@ namespace AIS.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof(API.Mapper.MappingProfile).Assembly, typeof(BLL.Mapper.MappingProfile).Assembly);
+            services.AddAutoMapper(typeof(Mapper.MappingProfile).Assembly, typeof(BLL.Mapper.MappingProfile).Assembly);
             services.AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.Load("AIS.API")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AIS.API", Version = "v1" });
@@ -43,7 +45,6 @@ namespace AIS.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AIS.API v1"));
             }
-            app.UseMiddleware<ExceptionMiddleWare>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
