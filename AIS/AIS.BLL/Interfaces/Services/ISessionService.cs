98f5db0ea3b7aad1f9aceabaff046bab33151c1b
GagarinPavel <ufufhby74@gmail.com>
2022-02-18 11:09:20 +0300

    change project files for test

--- AIS/AIS.BLL/Interfaces/Services/ISessionService.cs
+++ AIS/AIS.BLL/Interfaces/Services/ISessionService.cs
@@ -4,5 +4,8 @@ namespace AIS.BLL.Interfaces.Services
 {
     public interface ISessionService : IGenericService<Session>
     {
+        int MethodForTest1();
+        int MethodForTest2();
+        int MethodForTest3();
     }
 }
