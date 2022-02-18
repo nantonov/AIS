48f33ddf779c05100e68353b9e050b3232061070
GagarinPavel <ufufhby74@gmail.com>
2022-02-18 11:19:37 +0300

    remove unnecessary changes

--- AIS/AIS.BLL/Interfaces/Services/ISessionService.cs
+++ AIS/AIS.BLL/Interfaces/Services/ISessionService.cs
@@ -4,8 +4,5 @@ namespace AIS.BLL.Interfaces.Services
 {
     public interface ISessionService : IGenericService<Session>
     {
-        int MethodForTest1();
-        int MethodForTest2();
-        int MethodForTest3();
     }
 }
--- AIS/AIS.BLL/Services/SessionService.cs
+++ AIS/AIS.BLL/Services/SessionService.cs
@@ -11,47 +11,5 @@ namespace AIS.BLL.Services
         public SessionService(ISessionRepository repository, IMapper mapper) : base(repository, mapper)
         {
         }
-
-        public int MethodForTest1()
-        {
-            int i = 10;
-            int j = 0;
-            i++;
-            i--;
-            i-= 20;
-            for (;i<100;i++)
-            {
-                if (i % 2 > 0) j += i;
-            }
-            return j;
-        }
-
-        public int MethodForTest2()
-        {
-            int i = 10;
-            int j = 0;
-            i++;
-            i--;
-            i -= 20;
-            for (; i < 100; i++)
-            {
-                if (i % 2 == 0) j += i;
-            }
-            return j;
-        }
-
-        public int MethodForTest3()
-        {
-            int i = 10;
-            int j = 0;
-            i++;
-            i--;
-            i -= 100;
-            for (; i < 100; i++)
-            {
-                if (i < 0) j += i;
-            }
-            return j;
-        }
     }
 }
