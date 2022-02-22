2260f476e5239a34884ee178568868bfc0caf31c
GagarinPavel <ufufhby74@gmail.com>
2022-02-22 15:04:11 +0300

    add configs

---
+++ AIS.front/ais.front/src/config.js
@@ -0,0 +1,20 @@
+const config = {};
+
+if (process.env.APP_ENV === 'development'){
+  const devConfig = require('./config.dev.js').default;
+  Object.assign(config, devConfig);
+}
+else {
+  const localConfig = require('./config.local.js').default;
+  Object.assign(config, localConfig);
+}
+config.COMPANY_URL = 'api/Company';
+config.EMPLOYEE_URL = 'api/Employee';
+config.INTERVIEWEE_URL = 'api/Interviewee';
+config.QUESTION_URL = 'api/Question';
+config.QUESTION_AREA_URL = 'api/QuestionArea';
+config.QUESTION_INTERVIEWEE_ANSWER_URL = 'api/QuestionIntervieweeAnswer';
+config.QUESTION_SET_URL = 'api/QuestionSet';
+config.SESSION_URL = 'api/Session';
+config.TRUE_ANSWER_URL = 'api/TrueAnswer';
+export const Config = config;
