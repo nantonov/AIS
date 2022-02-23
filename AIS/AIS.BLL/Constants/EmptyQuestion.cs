405cfb08bfbb27ea8fae483627e8f6b0fc556d7f
GagarinPavel <ufufhby74@gmail.com>
2022-02-23 13:51:53 +0300

    add Empty Question

---
+++ AIS/AIS.BLL/Constants/EmptyQuestion.cs
@@ -0,0 +1,14 @@
+﻿using AIS.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS.BLL.Constants
+{
+    public static class EmptyQuestion
+    {
+        public static Question Empty = new() { Id = -1000};
+    }
+}
