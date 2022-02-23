337e196d65ec5dadabe0f36ced7f124343daee33
GagarinPavel <ufufhby74@gmail.com>
2022-02-23 13:52:13 +0300

    create NextQuestionController

---
+++ AIS/AIS.API/Controllers/NextQuestionController.cs
@@ -0,0 +1,31 @@
+﻿using AIS.API.ViewModels.Question;
+using AIS.BLL.Interfaces.Services;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NextQuestionController : ControllerBase
+    {
+        private readonly INextQuestionService _service;
+        private readonly IMapper _mapper;
+
+        public NextQuestionController(INextQuestionService service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<QuestionViewModel> next(int sessionId, int setId, CancellationToken ct)
+        {
+            var i = _mapper.Map<QuestionViewModel>(await _service.next(sessionId, setId, ct));
+            return i;
+        }
+    }
+}
