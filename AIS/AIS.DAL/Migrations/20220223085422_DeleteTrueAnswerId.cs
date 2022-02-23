dde52ac55a0a25c9f3b2857e0fccf1a468d88be7
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2022-02-23 11:55:57 +0300

    delete true answerid from Question Interviewee Answers

---
+++ AIS/AIS.DAL/Migrations/20220223085422_DeleteTrueAnswerId.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AIS.DAL.Migrations
+{
+    public partial class DeleteTrueAnswerId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionIntervieweeAnswers_TrueAnswers_TrueAnswerId",
+                table: "QuestionIntervieweeAnswers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_QuestionIntervieweeAnswers_TrueAnswerId",
+                table: "QuestionIntervieweeAnswers");
+
+            migrationBuilder.DropColumn(
+                name: "TrueAnswerId",
+                table: "QuestionIntervieweeAnswers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TrueAnswerId",
+                table: "QuestionIntervieweeAnswers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QuestionIntervieweeAnswers_TrueAnswerId",
+                table: "QuestionIntervieweeAnswers",
+                column: "TrueAnswerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionIntervieweeAnswers_TrueAnswers_TrueAnswerId",
+                table: "QuestionIntervieweeAnswers",
+                column: "TrueAnswerId",
+                principalTable: "TrueAnswers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
