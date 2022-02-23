4c965a7def9766617eba0230713fcc4ddc6e538f
GagarinPavel <ufufhby74@gmail.com>
2022-02-23 13:51:15 +0300

    create INextQuestionService and NextQuestionService

---
+++ AIS/AIS.BLL/Interfaces/Services/INextQuestionService.cs
@@ -0,0 +1,11 @@
+﻿using AIS.BLL.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIS.BLL.Interfaces.Services
+{
+    public interface INextQuestionService
+    {
+        Task<Question> next(int sessionId, int setId, CancellationToken ct);
+    }
+}
---
+++ AIS/AIS.BLL/Services/NextQuestionService.cs
@@ -0,0 +1,46 @@
+﻿using AIS.BLL.Constants;
+using AIS.BLL.Interfaces.Services;
+using AIS.BLL.Models;
+using AIS.DAL.Entities;
+using AIS.DAL.Interfaces.Repositories;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AIS.BLL.Services
+{
+    public class NextQuestionService: INextQuestionService
+    {
+        private readonly IGenericRepository<QuestionEntity> _questions;
+        private readonly IGenericRepository<QuestionIntervieweeAnswerEntity> _answers;
+        private readonly IMapper _mapper;
+
+        public NextQuestionService (IGenericRepository<QuestionEntity> questions,
+            IGenericRepository<QuestionIntervieweeAnswerEntity> answers,
+            IMapper mapper)
+        {
+            _questions = questions;
+            _answers = answers;
+            _mapper = mapper;
+        }
+
+        public async Task<Question> next(int sessionId, int setId, CancellationToken ct)
+        {
+            List<QuestionEntity> questions = (await _questions.Get(ct)).Where(item=> item.QuestionSetId == sessionId).ToList();
+            var answers = (await _answers.Get(ct)).Where(item=>item.SessionId==sessionId);
+
+            questions = questions.Where(question=> !answers.Any(answer=>answer.QuestionId==question.Id)).ToList();
+            if (questions.Count > 0)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(0, questions.Count);
+                return _mapper.Map<Question>(questions[index]);
+            }
+            return EmptyQuestion.Empty;
+        }
+    }
+}
