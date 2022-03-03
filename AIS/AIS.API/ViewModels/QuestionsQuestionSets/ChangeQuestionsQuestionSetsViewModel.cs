989e92534f769c7eaf56cac0b51d33f148c74f32
GagarinPavel <ufufhby74@gmail.com>
2022-03-03 15:10:04 +0300

    add view models

---
+++ AIS/AIS.API/ViewModels/QuestionsQuestionSets/ChangeQuestionsQuestionSetsViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace AIS.API.ViewModels.QuestionsQuestionSets
+{
+    public class ChangeQuestionsQuestionSetsViewModel
+    {
+        public int QuestionId { get; set; }
+        public int QuestionSetId { get; set; }
+    }
+}
---
+++ AIS/AIS.API/ViewModels/QuestionsQuestionSets/ShortQuestionsQuestionSetsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AIS.API.ViewModels.QuestionsQuestionSets
+{
+    public class ShortQuestionsQuestionSetsViewModel
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public int QuestionSetId { get; set; }
+    }
+}
