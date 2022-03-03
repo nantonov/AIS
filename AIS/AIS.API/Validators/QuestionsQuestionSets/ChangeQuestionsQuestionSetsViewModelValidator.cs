5ad31bf063185660ff231d3eccd1365c50ca73f3
GagarinPavel <ufufhby74@gmail.com>
2022-03-03 15:10:24 +0300

    add validators

---
+++ AIS/AIS.API/Validators/QuestionsQuestionSets/ChangeQuestionsQuestionSetsViewModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using AIS.API.ViewModels.QuestionsQuestionSets;
+using FluentValidation;
+
+namespace AIS.API.Validators.QuestionsQuestionSets
+{
+    public class ChangeQuestionsQuestionSetsViewModelValidator : AbstractValidator<ChangeQuestionsQuestionSetsViewModel>
+    {
+        public ChangeQuestionsQuestionSetsViewModelValidator()
+        {
+            RuleFor(x => x.QuestionSetId).NotNull().GreaterThan(0);
+            RuleFor(x => x.QuestionId).NotNull().GreaterThan(0);
+        }
+    }
+}
