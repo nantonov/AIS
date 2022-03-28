e30c4d73a28a8d978d715dbe4918e689bd0737a4
GagarinPavel <ufufhby74@gmail.com>
2022-03-28 11:52:18 +0300

    add eslint config

---
+++ AIS.front/ais.front/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'typescript-eslint/ban-types': 'off'
+  },
+};
