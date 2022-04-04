a2dd5b4ba66039a6bc995119eeb7a2cc31af660e
GagarinPavel <ufufhby74@gmail.com>
2022-04-04 12:49:15 +0300

    add tests

---
+++ AIS/AIS.API.Tests/ViewModels/ValidQuestionAreaViewModels.cs
@@ -0,0 +1,48 @@
+﻿using AIS.API.ViewModels.QuestionArea;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace AIS.API.Tests.ViewModels
+{
+    public static class ValidQuestionAreaViewModels
+    {
+        public static readonly QuestionAreaAddViewModel questionAreaAddViewModel = new()
+        {
+            Name = "Junior .NET",
+            QuestionSetsIds = new List<int> { 1, 2, 3, 4, 5 }
+        };
+
+        public static readonly QuestionAreaViewModel questionAreaViewModel = new()
+        {
+            Id = 1,
+            Name = "Junior .NET"
+        };
+
+        public static readonly QuestionAreaViewModel deleteQuestionAreaViewModel = new()
+        {
+            Id = 3,
+            Name = "Junior .NET"
+        };
+
+        public static readonly QuestionAreaUpdateViewModel questionAreaUpdateViewModel = new()
+        {
+            Name = "Junior .NET"
+        };
+
+        public static readonly ImmutableList<ShortQuestionAreaViewModel> questionAreasList =
+           new List<ShortQuestionAreaViewModel>()
+       {
+            new()
+            {
+                Name = "Junior .NET"
+            },
+            new()
+            {
+                Name = "Junior React"
+            }
+       }.ToImmutableList();
+
+        public static readonly IEnumerable<ShortQuestionAreaViewModel> questionAreasEmptyShortList =
+            new List<ShortQuestionAreaViewModel>();
+    }
+}
---
+++ AIS/AIS.BLL.Tests/Models/ValidQuestionAreaModels.cs
@@ -0,0 +1,46 @@
+﻿using AIS.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIS.BLL.Tests.Models
+{
+    public static class ValidQuestionAreaModels
+    {
+        public static readonly QuestionArea questionAreaModel = new()
+        {
+            Id = 1,
+            Name = "Juniour .NET",
+            QuestionSets = new List<QuestionSet>()
+            {
+                new()
+                {
+                    Id =1,
+                    Name = "OOP",
+                }
+            }
+        };
+
+        public static readonly QuestionArea questionAreaAddModel = new()
+        {
+            Name = "Juniour .NET",
+            QuestionSetsIds = new List<int> { 1, 5, 3 }
+        };
+
+        public static readonly IEnumerable<QuestionArea> questionAreaModelList = new List<QuestionArea>()
+        {
+            new()
+            {
+                Id = 1,
+                Name = "Juniour .NET"
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Juniour+ .NET"
+            }
+        };
+    }
+}
