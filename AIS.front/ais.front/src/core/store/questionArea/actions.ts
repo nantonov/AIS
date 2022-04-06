6cb43717322a1a25310037a71da72c133153f9a5
GagarinPavel <ufufhby74@gmail.com>
2022-04-06 15:56:17 +0300

    remove old store

--- AIS.front/ais.front/src/core/store/questionArea/actions.ts
+++
@@ -1,6 +0,0 @@
-import { createAction } from 'typesafe-actions';
-import { QuestionArea } from '../../interfaces/questionArea/questionArea';
-import { FETCH_ALL, FETCH_BY_ID } from './constants';
-
-export const fetchAll = createAction(FETCH_ALL)<QuestionArea[]>();
-export const fetchById = createAction(FETCH_BY_ID)<QuestionArea>();
