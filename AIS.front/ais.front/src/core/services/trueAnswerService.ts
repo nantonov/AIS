7f3b743ec2af62335ecdb5d959aeed41a3044838
GagarinPavel <ufufhby74@gmail.com>
2022-04-11 11:48:56 +0300

    refactor services

--- AIS.front/ais.front/src/core/services/trueAnswerService.ts
+++ AIS.front/ais.front/src/core/services/trueAnswerService.ts
@@ -3,38 +3,27 @@ import { TrueAnswer } from '../interfaces/trueAnswer/trueAnswer';
 import axiosInstance from '../../config/getAxious';
 import Config from '../../config/config';
 
-class TrueAnswerService {
-  public static async getAll(): Promise<TrueAnswer[]> {
-    const result = await axiosInstance
-      .get<TrueAnswer[]>(Config.TRUE_ANSWER_URL)
-      .then((res) => res.data);
+export const getAllTrueAnswersService = async (): Promise<TrueAnswer[]> => {
+  const result = await axiosInstance
+    .get<TrueAnswer[]>(Config.TRUE_ANSWER_URL)
+    .then((res) => res.data);
 
-    return result || [];
-  }
+  return result || [];
+};
 
-  public static async getById(trueAnswerId: number): Promise<TrueAnswer> {
-    const result = await axiosInstance
-      .get<TrueAnswer>(`${Config.TRUE_ANSWER_URL}/${trueAnswerId}`)
-      .then((res) => res.data);
+export const getTrueAnswerByIdService = async (trueAnswerId: number): Promise<TrueAnswer> => {
+  const result = await axiosInstance
+    .get<TrueAnswer>(`${Config.TRUE_ANSWER_URL}/${trueAnswerId}`)
+    .then((res) => res.data);
 
-    return result || defaultTrueAnswer;
-  }
+  return result || defaultTrueAnswer;
+};
 
-  public static deleteById(trueAnswerId: number): Promise<any> {
-    return axiosInstance.delete(`${Config.TRUE_ANSWER_URL}/${trueAnswerId}`);
-  }
+export const deleteTrueAnswerService = (trueAnswerId: number): Promise<any> =>
+  axiosInstance.delete(`${Config.TRUE_ANSWER_URL}/${trueAnswerId}`);
 
-  public static create(trueAnswer: TrueAnswer): Promise<any> {
-    return axiosInstance.post(Config.TRUE_ANSWER_URL, { ...trueAnswer });
-  }
+export const createTrueAnswerService = (trueAnswer: TrueAnswer): Promise<any> =>
+  axiosInstance.post(Config.TRUE_ANSWER_URL, { ...trueAnswer });
 
-  public static update(trueAnswer: TrueAnswer): Promise<boolean> {
-    return axiosInstance.put(
-      Config.TRUE_ANSWER_URL,
-      { ...trueAnswer },
-      { params: { id: trueAnswer.id } }
-    );
-  }
-}
-
-export default TrueAnswerService;
+export const updateTrueAnswerService = (trueAnswer: TrueAnswer): Promise<boolean> =>
+  axiosInstance.put(Config.TRUE_ANSWER_URL, { ...trueAnswer }, { params: { id: trueAnswer.id } });
