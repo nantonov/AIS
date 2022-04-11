8b0b8ce568ccf0fe494dbaf5b634ab90ba8be254
DZIANIS KAHUKIN <dzianis.kahukin@innowise-group.com>
2022-04-11 14:19:59 +0300

    Add function for delete questionSet

--- AIS.front/ais.front/src/pages/questionsSets/questionSetItem/QuestionSetItem.tsx
+++ AIS.front/ais.front/src/pages/questionsSets/questionSetItem/QuestionSetItem.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { QuestionSet } from '../../../core/interfaces/questionSet/questionSet';
 import QuestionSetContainer from '../../../core/components/itemContainer/ItemContainer';
-import { QuestionSetDescriptionDiv, QuestionSetHeader, QuestionSetNameText } from './style';
+import { QuestionSetDescriptionDiv, QuestionSetHeader, QuestionSetNameText } from './styled/style';
 import MainRoutes from '../../../core/constants/mainRoutes';
+import { DeleteIconButtonContainer } from '../../../core/components/deleteIconButtonContainer/DeleteIconButtonContainer';
+import { IconButtonContainer } from '../../../core/components/iconButtonContainer/IconButtonContainer';
+import { deleteQuestionSetById } from '../../../core/redux/thunk/questionSetThunk';
 
 interface Props {
   questionSet: QuestionSet;
@@ -11,15 +15,25 @@ interface Props {
 
 const QuestionSetItem: React.FC<Props> = ({ questionSet }) => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const routeChange = (id: number) => () => {
     const path = `/${MainRoutes.questionSet}/${id}`;
     navigate(path);
   };
+
+  const DeleteQuestionSetById = (questionSetId: number) => () => {
+    dispatch(deleteQuestionSetById(questionSetId));
+  };
+
   return (
     <QuestionSetContainer onClick={routeChange(questionSet.id)}>
       <QuestionSetHeader>{questionSet.name}</QuestionSetHeader>
       <QuestionSetDescriptionDiv>
         <QuestionSetNameText>{questionSet.name}</QuestionSetNameText>
+        <IconButtonContainer aria-label="delete">
+          <DeleteIconButtonContainer onClick={DeleteQuestionSetById(questionSet.id)} />
+        </IconButtonContainer>
       </QuestionSetDescriptionDiv>
     </QuestionSetContainer>
   );
